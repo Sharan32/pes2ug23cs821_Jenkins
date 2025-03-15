c8c6029fc380e761a3fc56e92ca6ab82ca393019
Sharan32 <168652976+Sharan32@users.noreply.github.com>
2025-03-15 21:08:08 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World NEW CHANGE\n";
+  cout << " NEW Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   return 0;
